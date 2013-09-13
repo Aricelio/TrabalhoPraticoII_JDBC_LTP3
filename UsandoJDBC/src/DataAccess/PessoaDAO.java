f72ed7b73e3e39557ec76f2e5139e7c0a69a276a
alisson 02 <alisson 02@192.168.0.2>
2013-09-13 00:04:56 -0300

    Classe Pronta

--- UsandoJDBC/src/DataAccess/PessoaDAO.java
+++ UsandoJDBC/src/DataAccess/PessoaDAO.java
@@ -12,41 +12,53 @@ import java.sql.PreparedStatement;
  *
  * @author ALUNO
  */
-public class PessoaDAO extends DAO 
-{
-    public PessoaDAO()
-    {
+public class PessoaDAO extends DAO {
+
+    public PessoaDAO() {
         super();
     }
-    
-    public boolean Salvar(Pessoa obj) 
-    {
-        if (obj.getCodigo() == 0) 
-        {
-            try 
-            {
+    /*--------------------- Método Salvar ------------------------------------*/
+
+    public boolean Salvar(Pessoa obj) {
+        if (obj.getCodigo() == 0) {
+            try {
                 PreparedStatement sql = getConexao().prepareStatement("insert into pessoa(Nome,DataNascimento) values(?,?)");
                 sql.setString(1, obj.getNome());
-                sql.setDate(2, new java.sql.Date( obj.getDataNascimento().getTime() ));
+                sql.setDate(2, new java.sql.Date(obj.getDataNascimento().getTime()));
                 sql.executeUpdate();
                 return true;
 
-            } 
-            catch (Exception ex) 
-            {
+            } catch (Exception ex) {
                 System.err.println(ex.getMessage());
                 return false;
             }
-        } 
-        else 
-        {
-            try 
-            {
+        } else {
+            try {
                 Connection con = getConexao();
                 PreparedStatement sql = con.prepareStatement("update Pessoa set Nome=?, DataNascimento=? where codPessoa=?");
                 sql.setString(1, obj.getNome());
-                sql.setDate(2, new java.sql.Date( obj.getDataNascimento().getTime() ));
+                sql.setDate(2, new java.sql.Date(obj.getDataNascimento().getTime()));
                 sql.setInt(3, obj.getCodigo());
+                sql.executeUpdate();
+                return true;
+
+            } catch (Exception ex) {
+                System.err.println(ex.getMessage());
+                return false;
+            }
+        }
+    }
+    /*----------------------- Método Remover ---------------------------------*/
+
+    public boolean Remover(Pessoa obj) 
+    {
+        if (obj.getCodigo() >= 0) 
+        {
+            try 
+            {
+                PreparedStatement sql = getConexao().prepareStatement("delete from pessoa where codPessoa=?");
+                sql.setInt(1, obj.getCodigo());
+                //sql.setDate(2, new java.sql.Date( obj.getDataNascimento().getTime() ));
                 sql.executeUpdate();
                 return true;
 
@@ -57,6 +69,6 @@ public class PessoaDAO extends DAO
                 return false;
             }
         }
-    } 
+        return true;
+    }
 }
-
