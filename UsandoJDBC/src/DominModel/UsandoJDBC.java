e6970771fbb00c21c6bfd7c4696608ca59e2988a
alisson 02 <alisson 02@192.168.0.2>
2013-09-12 23:22:09 -0300

    Classe Pronta

---
+++ UsandoJDBC/src/DominModel/UsandoJDBC.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DominModel;
+
+/**
+ *
+ * @author alisson 02
+ */
+public class UsandoJDBC {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+    }
+}
