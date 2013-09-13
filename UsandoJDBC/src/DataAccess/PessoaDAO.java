77ac4b6aca00a7eeab205aa28df80820c8c2aa42
alisson 02 <alisson 02@192.168.0.2>
2013-09-12 23:42:04 -0300

    Classe Pronta

---
+++ UsandoJDBC/src/DataAccess/PessoaDAO.java
@@ -0,0 +1,62 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DataAccess;
+
+import DominModel.*; //Importando o Pacote DominModel
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+/**
+ *
+ * @author ALUNO
+ */
+public class PessoaDAO extends DAO 
+{
+    public PessoaDAO()
+    {
+        super();
+    }
+    
+    public boolean Salvar(Pessoa obj) 
+    {
+        if (obj.getCodigo() == 0) 
+        {
+            try 
+            {
+                PreparedStatement sql = getConexao().prepareStatement("insert into pessoa(Nome,DataNascimento) values(?,?)");
+                sql.setString(1, obj.getNome());
+                sql.setDate(2, new java.sql.Date( obj.getDataNascimento().getTime() ));
+                sql.executeUpdate();
+                return true;
+
+            } 
+            catch (Exception ex) 
+            {
+                System.err.println(ex.getMessage());
+                return false;
+            }
+        } 
+        else 
+        {
+            try 
+            {
+                Connection con = getConexao();
+                PreparedStatement sql = con.prepareStatement("update Pessoa set Nome=?, DataNascimento=? where codPessoa=?");
+                sql.setString(1, obj.getNome());
+                sql.setDate(2, new java.sql.Date( obj.getDataNascimento().getTime() ));
+                sql.setInt(3, obj.getCodigo());
+                sql.executeUpdate();
+                return true;
+
+            } 
+            catch (Exception ex) 
+            {
+                System.err.println(ex.getMessage());
+                return false;
+            }
+        }
+    } 
+}
+
