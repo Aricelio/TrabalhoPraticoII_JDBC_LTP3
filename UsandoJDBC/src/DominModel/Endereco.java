81d497a1384ea9076cfe0d1a1ada5f7b77fcb13e
alisson 02 <alisson 02@192.168.0.2>
2013-09-20 17:26:42 -0300

    Mensagens de erro adicionadas

--- UsandoJDBC/src/DominModel/Endereco.java
+++ UsandoJDBC/src/DominModel/Endereco.java
@@ -54,60 +54,81 @@ public class Endereco
     }
 
     /*----------Métodos Setters------------------------------*/
-    public void setCodigo(int codigo) {
+    public void setCodigo(int codigo) throws Exception{
        if(codigo >= 1)
          this.codigo = codigo;
+       else{
+         throw new Exception("Codigo Invalido!");
+       }
     }
     
-    public void setNumero(int numero) 
+    public void setNumero(int numero) throws Exception
     {        
          if(numero >= 1)
-            this.numero = numero;        
+            this.numero = numero; 
+         else{
+            throw new Exception("Valor passado para o campo 'Numero' é Invalido!");
+         }
     }
 
-    public void setRua(String rua) 
+    public void setRua(String rua) throws Exception
     {        
-        Pattern Rua = Pattern.compile("\\w*{3,}");
+        Pattern Rua = Pattern.compile("[a-zA-Z0-9 ]*");
         Matcher verifica = Rua.matcher(rua);
         
         if(verifica.matches())
-            this.rua = rua;        
+            this.rua = rua;   
+        else{
+            throw new Exception("Valor passado para o campo 'Rua' é Invalido!");
+         }
     }
 
-    public void setBairro(String bairro) 
+    public void setBairro(String bairro)  throws Exception
     {        
-        Pattern Bairro = Pattern.compile("\\w*{3,}");
+        Pattern Bairro = Pattern.compile("[a-zA-Z0-9 ]*");
         Matcher verifica = Bairro.matcher(bairro);
         
         if(verifica.matches())        
              this.bairro = bairro;
+        else{
+            throw new Exception("Valor passado para o campo 'Bairro' é Invalido!");
+         }
     }
 
-    public void setCidade(String cidade) 
+    public void setCidade(String cidade) throws Exception 
     {
-        Pattern Cidade = Pattern.compile("\\w*{3,}");
+        Pattern Cidade = Pattern.compile("[a-zA-Z0-9 ]*");
         Matcher verifica = Cidade.matcher(cidade);
         
         if(verifica.matches())
              this.cidade = cidade;
+        else{
+            throw new Exception("Valor passado para o campo 'Cidade' é Invalido!");
+         }
     }
 
-    public void setUf(String uf) 
+    public void setUf(String uf)throws Exception  
     {
-        Pattern UF = Pattern.compile("\\w*{3,}");
+        Pattern UF = Pattern.compile("\\w*{2,}");
         Matcher verifica = UF.matcher(uf);
         
         if(verifica.matches())
              this.uf = uf;
+        else{
+            throw new Exception("Valor passado para o campo 'UF' é Invalido!");
+         }
     }
 
-    public void setPais(String pais) 
+    public void setPais(String pais)throws Exception
     {
-        Pattern Pais = Pattern.compile("\\w*{3,}");
+        Pattern Pais = Pattern.compile("[a-zA-Z0-9 ]*");
         Matcher verifica = Pais.matcher(pais);
         
         if(verifica.matches())
              this.pais = pais;
+        else{
+            throw new Exception("Valor passado para o campo 'Pais' é Invalido!");
+        }
     }
  
 }
