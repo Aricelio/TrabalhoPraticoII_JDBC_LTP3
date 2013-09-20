c42828996348f3351b622ea200b4481064e527db
alisson 02 <alisson 02@192.168.0.2>
2013-09-20 17:26:54 -0300

    Mensagens de erro adicionadas

--- UsandoJDBC/src/DominModel/Produto.java
+++ UsandoJDBC/src/DominModel/Produto.java
@@ -36,31 +36,44 @@ public class Produto {
     }
 
     /*-------- Setters--------------------------------------------------------*/
-    public void setCodigo(int codigo) {
+    public void setCodigo(int codigo) throws Exception
+    {
         if(codigo >= 1)
             this.codigo = codigo;
+        else{
+            throw new Exception("Valor passado para o campo 'Codigo' é invalido!Valor menor que 1");
+        }
     }
 
-    public void setNome(String nome) 
+    public void setNome(String nome)throws Exception 
     {
         Pattern Nome = Pattern.compile("[0-9\\w\\s]{3,}");
         Matcher verifica = Nome.matcher(nome);
         
         if(verifica.matches())
             this.nome = nome;
+        else{
+            throw new Exception("Valor passado para o campo 'Nome' é invalido!");
+        }
     }
 
-    public void setTipo(String tipo) 
+    public void setTipo(String tipo) throws Exception
     {
         Pattern Tipo = Pattern.compile("[0-9\\w\\s]{3,}");
         Matcher verifica = Tipo.matcher(tipo);
         
         if(verifica.matches())
             this.tipo = tipo;
+        else{
+            throw new Exception("Valor passado para o campo 'Tipo' é invalido!");
+        }
     }
-    public void setPreco(double preco) 
+    public void setPreco(double preco)throws Exception 
     {        
         if(preco >= 1)
             this.preco = preco;
+        else{
+            throw new Exception("Valor passado para o campo 'Preoc' é invalido!Valor menor que 1");
+        }
     }
 }
